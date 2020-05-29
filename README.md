86f052d666059527abdebf324ea6ea6073259175
wcv007 <varunsingla2007@gmail.com>
2020-05-29 12:06:41 +0530

    added images-n

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# React File Upload
-
-> This is a full stack React-Express file uploader but could easily be modified to work with any back-end including cloud storage
-
-## Quick Start
-
 ```bash
 # Install dependencies server/client
 npm install
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Message = ({ msg }) => {
   return (
-    <div className='alert alert-info alert-dismissible fade show' role='alert'>
+    <div
+      className="alert alert-primary alert-dismissible fade show"
+      role="alert"
+    >
       {msg}
       <button
-        type='button'
-        className='close'
-        data-dismiss='alert'
-        aria-label='Close'
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
       >
-        <span aria-hidden='true'>&times;</span>
+        <span aria-hidden="true">&times;</span>
       </button>
     </div>
   );
 };
 
 Message.propTypes = {
-  msg: PropTypes.string.isRequired
+  msg: PropTypes.string.isRequired,
 };
 
 export default Message;
